cb55e8e4dc4557a1bfce0dccdde31b21ad1c9c98
William Littre <littrewilliam@gmail.com>
2026-03-18 16:29:28 +0100

    debut de la partie 6 fait

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import jeu.JeuDeLaVie;
 import jeu.JeuDeLaVieUI;
 import visiteur.VisiteurClassique;
+import observateur.*;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         JeuDeLaVie jeu = new JeuDeLaVie();
         JeuDeLaVieUI ui = new JeuDeLaVieUI(jeu);
+        ObservateurConsole console = new ObservateurConsole(jeu);
         jeu.attacheObservateur(ui);
         jeu.setVisiteur(new VisiteurClassique(jeu));
         jeu.notifieObservateurs();
